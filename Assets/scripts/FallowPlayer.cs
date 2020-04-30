5f98ed9963d7a59e6fbf62955ae0a77a8dc7f341
MW <waiting4marks@gmail.com>
2020-04-30 14:52:20 +0200

    lesson 1-8

---
+++ Assets/scripts/FallowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3  offset;
+
+    void Update()
+    {
+    
+           //Debug.Log(player.position); 
+           transform.position = player.position + offset;
+    }
+}
---
+++ Assets/scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+  
+    public Transform player;
+    public Text scoreText;
+    // Update is called once per frame
+    void Update()
+    {
+    double result = player.position.z + 30;
+    scoreText.text = result.ToString("0");
+        //player.position.z;
+    }
+}
